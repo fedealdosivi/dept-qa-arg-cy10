dc7465d08148d14b4bf5c1ab3613ac755afb1f16
fedealdosivi <fedepalo13@gmail.com>
2022-10-07 13:02:26 -0300

    added intercept

--- cypress/e2e/dept-qa-challenge/spacex.cy.js
+++ cypress/e2e/dept-qa-challenge/spacex.cy.js
@@ -2,7 +2,12 @@
 
 describe('CHALLENGE SPACEX', () => {
     beforeEach(() => {
+        cy.intercept('https://api.spacexdata.com/v3/rockets').as('getRockets')
         cy.visit('http://csb-x6dpt1.netlify.app/')
+        cy.wait('@getRockets').then(intercept => {
+            cy.log(intercept.response.body);
+            expect(Array[4], intercept.response.body);
+        })
     })
 
     it('Checks home page loads', () => {
